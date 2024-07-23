3cd7eae1bcc479081cf7a9d0320774ed7c226ecd
NiharP31 <niharasare@gmail.com>
2024-07-22 22:42:26 -0400

    initial commit

---
+++ data_loader.py
@@ -0,0 +1,4 @@
+from imports import *
+
+train_data = pd.read_csv(r'C:\Users\nihar\Documents\github\Deep_learning\MNIST\dataset\train\train.csv')
+test_data = pd.read_csv(r'C:\Users\nihar\Documents\github\Deep_learning\MNIST\dataset\test\test.csv')
---
+++ imports.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader, Dataset
